4e56580581ab1a26e69d8d82a4daa8e0251cb77f
Gyraxy <you@example.com>
2019-04-06 18:04:21 +0200

    Added colors theme + Toolbar + BottomNavigation

--- app/src/main/java/com/nicolas/duboscq/realestatemanager/MainActivity.kt
+++ app/src/main/java/com/nicolas/duboscq/realestatemanager/MainActivity.kt
@@ -2,11 +2,38 @@ package com.nicolas.duboscq.realestatemanager
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.support.design.widget.BottomNavigationView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        configureToolBar()
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_activity_toolbar, menu)
+        return true
+    }
+
+    private fun configureToolBar() {
+        setSupportActionBar(activity_main_toolbar)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_list -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_map -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
