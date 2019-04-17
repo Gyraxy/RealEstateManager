0da401654086185d24bb1170b44cb63b7106caf0
Gyraxy <you@example.com>
2019-04-17 12:03:22 +0200

    Added : dimens file/device compatibility SearchActivity Layout/style to format layout

--- app/src/main/java/com/nicolas/duboscq/realestatemanager/SearchActivity.kt
+++ app/src/main/java/com/nicolas/duboscq/realestatemanager/SearchActivity.kt
@@ -1,19 +1,31 @@
 package com.nicolas.duboscq.realestatemanager
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
-import android.view.View
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_search.*
+import java.util.*
+import android.graphics.Paint.UNDERLINE_TEXT_FLAG
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.simple_spinner_item.view.*
 
-class SearchActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+class SearchActivity : AppCompatActivity(){
+
+    private val searchType = arrayOf("Aucun", "Appartement", "Maison", "Duplex", "Penthouse")
+    private val searchPrice = arrayOf("0", "10000", "50000", "100000", "500000", "1000000", "2000000")
+    private val searchSurface = arrayOf("0", "50", "100", "150", "200", "250", "300")
+    private val searchStatus = arrayOf("Aucun", "A Vendre", "Vendu")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         configureToolBar()
         configureAllSpinner()
+        configureDatePicker()
+        activity_search_btn.setOnClickListener { Toast.makeText(this,activity_search_type_sp.spinner_text.text,Toast.LENGTH_SHORT).show() }
     }
 
     private fun configureToolBar() {
@@ -28,29 +40,44 @@ class SearchActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     }
 
     // SPINNER CONFIGURATION
-    private fun configureSpinner(idRStringArray: Int,spinner: Spinner){
+    private fun configureSpinner(idRStringArray: Array<String>,spinner: Spinner){
 
-        val adapter = ArrayAdapter.createFromResource(this, idRStringArray, android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
-        spinner.setAdapter(adapter)
+        val spinnerAdapter = ArrayAdapter<String>(this, R.layout.simple_spinner_item,idRStringArray)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+        spinner.adapter = spinnerAdapter
         spinner.onItemSelectedListener
     }
 
     private fun configureAllSpinner(){
-        this.configureSpinner(R.array.search_type,activity_search_type_sp)
-        this.configureSpinner(R.array.search_price,activity_search_price_min_sp)
-        this.configureSpinner(R.array.search_price,activity_search_price_max_sp)
-        this.configureSpinner(R.array.search_surface,activity_search_surf_min_sp)
-        this.configureSpinner(R.array.search_surface,activity_search_surf_max_sp)
-        this.configureSpinner(R.array.search_status,activity_search_status_sp)
+        this.configureSpinner(searchType,activity_search_type_sp)
+        this.configureSpinner(searchPrice,activity_search_price_min_sp)
+        this.configureSpinner(searchPrice,activity_search_price_max_sp)
+        this.configureSpinner(searchSurface,activity_search_surf_min_sp)
+        this.configureSpinner(searchSurface,activity_search_surf_max_sp)
+        this.configureSpinner(searchStatus,activity_search_status_sp)
     }
 
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            when(parent!!.id) {
-            }
+    private fun configureDatePicker(){
+        underlineEditText(activity_search_date_min_edt)
+        underlineEditText(activity_search_date_max_edt)
+        activity_search_date_min_edt.setOnClickListener{showDatePicker(activity_search_date_min_edt)}
+        activity_search_date_max_edt.setOnClickListener{showDatePicker(activity_search_date_max_edt)}
     }
 
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun showDatePicker(idRDateEditText: TextView){
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            val date = "$dayOfMonth/$month/$year"
+            idRDateEditText.text = date
+        }, year, month, day)
+        dpd.show()
+    }
+
+    private fun underlineEditText(idRDateEditText: TextView){
+        idRDateEditText.paintFlags = UNDERLINE_TEXT_FLAG
+        idRDateEditText.text = ("                          ")
     }
 }
